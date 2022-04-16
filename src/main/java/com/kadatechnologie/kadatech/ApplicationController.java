d93bfc6403af232a203603a3b2a33aa819551c5c
Sieky <jerometounkara@gmail.com>
2022-04-16 19:39:35 +0000

    Final templates kadatech

--- src/main/java/com/kadatechnologie/kadatech/ApplicationController.java
+++ src/main/java/com/kadatechnologie/kadatech/ApplicationController.java
@@ -9,7 +9,28 @@ public class ApplicationController {
 	@GetMapping("/index")
 	public String home() { return "index"; }
 	
-	@GetMapping("/pages-blank")
-	public String pages() { return "pages-blank"; }
+	@GetMapping("/blog")
+	public String blog() { return "blog"; }
+	
+	@GetMapping("/cart")
+	public String cart() { return "cart"; }
+	
+	@GetMapping("/contact-us")
+	public String contact() { return "contact-us"; }
+	
+	@GetMapping("/login")
+	public String login() { return "login"; }
+	
+	@GetMapping("/product-details")
+	public String product() { return "product-details"; }
+	
+	@GetMapping("/shop")
+	public String shop() { return "shop"; }
+	
+	@GetMapping("/404")
+	public String error() { return "404"; }
+	
+	@GetMapping("/checkout")
+	public String checkout() { return "checkout"; }
 
 }
